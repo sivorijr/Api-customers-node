d2a2d1ec4d69b35c341832b1eaa7a1bb4294eb63
Sivori Junior <sivori_jr@hotmail.com>
2020-05-09 17:38:27 -0300

    Config heroku

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(require("./routes"));
 app.use(cors());
 
-mongoose.connect("mongodb+srv://root:root@cluster-l4tpr.gcp.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
