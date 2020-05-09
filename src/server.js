3a379fdbcb14867e1f4bb432a497f77b0025e92f
Sivori Junior <sivori_jr@hotmail.com>
2020-05-09 18:14:29 -0300

    Teste config heroku

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
 
-app.use(bodyParser.json());
-app.use(require("./routes"));
-app.use(cors());
+const app = express();
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
 });
+
+// app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require("./routes"));
 
 app.listen(process.env.PORT || 3434);
