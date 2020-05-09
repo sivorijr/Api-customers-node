0ab11a8f8540fdbc8133b624b3fa99fb4ede6835
Sivori Junior <sivori_jr@hotmail.com>
2020-05-09 19:39:06 -0300

    Ajustes server

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
-
 const app = express();
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-// app.use(bodyParser.json());
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(require("./routes"));
 
-app.listen(process.env.PORT || 3434);
+app.listen(process.env.PORT || 3333);
