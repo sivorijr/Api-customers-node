5c84382852862028fb0e7ebd40fbd0b10745bb39
Sivori Junior <sivori_jr@hotmail.com>
2020-05-09 17:24:57 -0300

    Config server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(require("./routes"));
+app.use(cors());
 
 mongoose.connect("mongodb+srv://root:root@cluster-l4tpr.gcp.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true,
