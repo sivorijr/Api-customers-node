cb4a13ca3eb1d30c84a788b39d3993218a084799
Sivori Junior <sivori_jr@hotmail.com>
2021-03-21 15:37:36 -0300

    Add health

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const routes = express.Router();
 const CustomerController = require("../controllers/CustomerController");
 
 routes.get("/", (req, res) => { return res.send("CUSTOMERS API by Sivori Junior") });
+routes.get("/health", (req, res) => { return res.send("OK") });
 
 routes.get("/customers", CustomerController.getAll);
 
