795dcfe1524bf4b4389191cd25e994ef68b07780
Sivori Junior <sivori_jr@hotmail.com>
2021-03-21 21:33:29 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,6 @@ const routes = express.Router();
 const CustomerController = require("../controllers/CustomerController");
 
 routes.get("/", (req, res) => { return res.send("CUSTOMERS API by Sivori Junior") });
-routes.get("/health", (req, res) => { return res.send("OK") });
 
 routes.get("/customers", CustomerController.getAll);
 
